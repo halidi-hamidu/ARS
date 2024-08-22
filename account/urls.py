08780aabd728e004641c2a17ff0c41ed6c89f961
Absamtz <abuysalim93@gmail.com>
2024-08-22 15:19:16 +0300

    url

--- account/urls.py
+++ account/urls.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import ApartmentPage
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
 
 
 app_name = 'account'
@@ -14,7 +12,3 @@ urlpatterns = [
     path('Apartment/', ApartmentPage.as_view(), name='apartment')
     
 ]
-
-urlpatterns = [
-    # ... your url patterns ...
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
